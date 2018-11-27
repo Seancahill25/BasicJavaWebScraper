6c33a2a74eca4115d5c21b83ec293f0aa9563db5
Sean Cahill <seancahill25@gmail.com>
2018-11-26 23:43:08 -0500

    Initial commit

---
+++ src/main/java/org/openqa/selenium/example/Scraper.java
@@ -0,0 +1,36 @@
+package org.openqa.selenium.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Scraper  {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://www.google.com");
+
+        WebElement element = driver.findElement(By.name("q"));
+
+        element.sendKeys("Cheese!");
+
+        element.submit();
+
+        System.out.println("Page title is: " + driver.getTitle());
+
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("cheese!");
+            }
+        });
+
+        System.out.println("Page title is: " + driver.getTitle());
+
+        driver.quit();
+    }
+}
